061f7d794025c7b758f67282bc2761731ac0ea85
Dipak <dipakbarman080@gmail.com>
2024-04-24 16:41:18 +0530

    carinsert

--- server.js
+++ server.js
@@ -4,14 +4,22 @@ const cors = require("cors");
 const app = express();
 const authrouter = require('./router/auth-router');
 const carrouter = require('./router/car-router');
+const cityrouter = require('./router/city-router');
 const errorMiddleware = require("./middlewares/error-middleware");
 const connectDb = require("./utils/db");
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth",authrouter);
 app.use("/api/car",carrouter);
+app.use("/api/city",cityrouter);
 app.use(errorMiddleware);
 const PORT = 5000;
+app.get('/', (req, res) => {
+    // Get the hostname of the incoming request
+    const hostname = req.hostname;
+  
+    res.send(`Hostname: ${hostname}`);
+  });
 connectDb().then(()=>{
     app.listen(PORT,()=>{
         console.log(`'server is running  at port : ' ${PORT}`);
